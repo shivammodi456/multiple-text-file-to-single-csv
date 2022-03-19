66b00df69b590a264d79722dbb1e7134065f8bec
Shivam Modi <shivammodi@Shivams-MacBook-Air.local>
2022-03-19 22:36:28 +0530

    unwanted dependencies removed

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 const fs = require("fs");
-const lodash = require("lodash");
-//requiring path and fs modules
 const path = require("path");
-//joining path of directory
-const finalData = [];
-const directoryPath = path.join(__dirname, "/textFile");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const uniqeKeys = [];
-const removedLines = [1, 2, 3];
-const removedKeys = removedLines.map((key) => `name--${key}`);
-//passsing directoryPath and callback function
-const files = fs.readdirSync(directoryPath);
+const allFileData = [];
+const removedLines = []; //insert number of line which needs to excluded eg :- [1,3,4]
 
-files.forEach(function (file, i) {
-  const data = require("fs")
-    .readFileSync(`./textFile/${file}`, "utf-8")
+const removedKeys = removedLines.map((key) => `field-${key}`);
+const files = fs.readdirSync(path.join(__dirname, "/textFiles"));
+
+files.forEach(function (file) {
+  const singleFileObj = {};
+  const singleFileData = fs
+    .readFileSync(path.join(__dirname, `/textFiles/${file}`), "utf-8")
     .split(/\r?\n/);
-  const data1 = lodash.mapKeys(data, function (value, key) {
-    return "name--" + key;
-  });
-  finalData.push(data1);
+  singleFileData.forEach((key, i) => (singleFileObj[`field-${i}`] = key));
+  allFileData.push(singleFileObj);
 });
 
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-function createCSV() {
-  finalData.forEach((array) => {
-    lodash.forIn(array, function (value, key) {
-      if (!uniqeKeys.includes(key) && removedKeys.indexOf(key) < 0) {
-        uniqeKeys.push(key);
-      }
-    });
+allFileData.forEach((obj) => {
+  Object.keys(obj).forEach((key) => {
+    if (!uniqeKeys.includes(key) && removedKeys.indexOf(key) < 0) {
+      uniqeKeys.push(key);
+    }
   });
-  const headers = uniqeKeys.map((uniqK) => {
-    return { id: uniqK, title: uniqK };
-  });
-  const csvWriter = createCsvWriter({
-    path: "file.csv",
-    header: headers,
-  });
-  csvWriter.writeRecords(finalData).then(() => {
-    console.log("...Done");
-  });
-}
-createCSV();
+});
+const headers = uniqeKeys.map((uniqK) => {
+  return { id: uniqK, title: uniqK };
+});
+const csvWriter = createCsvWriter({
+  path: "Output.csv",
+  header: headers,
+});
+csvWriter.writeRecords(allFileData).then(() => {
+  console.log("CSV Generated.....");
+});
